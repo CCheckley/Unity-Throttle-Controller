2faf205e21d5229bb10cd04184d4f65171826247
unknown <connor.checkley@gmail.com>
2020-01-18 22:09:36 +0000

    Initial Commit

---
+++ ThrottleController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class ThrottleController : MonoBehaviour
+{
+    new Rigidbody2D rigidbody2D;
+
+    [SerializeField] float maxAngle = 180.0f;
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Rotate();
+    }
+
+    void Rotate()
+    {
+        rigidbody2D.rotation = maxAngle * Input.GetAxisRaw("Throttle");
+    }
+}
