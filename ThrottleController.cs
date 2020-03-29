3b96c3d4b0aa9055c0a07f622954d6379c5326a8
unknown <connor.checkley@gmail.com>
2020-03-29 12:50:49 +0100

    added comments, setup rigidbody, removed unecessary usings

--- ThrottleController.cs
+++ ThrottleController.cs
@@ -1,17 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+// Ensure the component is present on the gameobject the script is attached to
 [RequireComponent(typeof(Rigidbody2D))]
 public class ThrottleController : MonoBehaviour
 {
+    // Local rigidbody variable to hold a reference to the attached Rigidbody2D component
     new Rigidbody2D rigidbody2D;
 
     [SerializeField] float maxAngle = 180.0f;
 
-    void Start()
+    void Awake()
     {
+        // Setup Rigidbody for frictionless top down movement and dynamic collision
         rigidbody2D = GetComponent<Rigidbody2D>();
+
+        rigidbody2D.isKinematic = false;
+        rigidbody2D.angularDrag = 0.0f;
+        rigidbody2D.gravityScale = 0.0f;
     }
 
     void Update()
@@ -21,6 +26,7 @@ public class ThrottleController : MonoBehaviour
 
     void Rotate()
     {
+        // Set rigidbody rotation
         rigidbody2D.rotation = maxAngle * Input.GetAxisRaw("Throttle");
     }
 }
